672f84824b61c9b17b6bd2d3756de0f943227f57
Umang Pokhriyal <umangpokhriyall@gmail.com>
2025-01-25 14:31:48 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+
+import express from "express"
+import mongoose from "mongoose"
+
+const app = express()
+
+app.post("api/v1/signup", (req, res) => {
+
+})
+app.post("api/v1/signin", (req, res) => {
+
+})
+app.post("api/v1/content", (req, res) => {
+
+})
+app.get("api/v1/content", (req, res) => {
+
+})
+app.delete("api/v1/content", (req, res) => {
+
+})
+app.post("api/v1/brain/share", (req, res) => {
+
+})
+app.get("api/v1/brain/:shareLink", (req, res) => {
+
+})
